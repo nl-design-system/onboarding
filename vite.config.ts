9ee5baacf88a5e55bdea537473eb860ccca0eb31
Robbert Broersma <git@robbertbroersma.nl>
2024-10-17 08:32:05 +0200

    build: configure base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const BASE_URL = (process as never) && process.env["BASE_URL"];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: BASE_URL || "/",
+});
